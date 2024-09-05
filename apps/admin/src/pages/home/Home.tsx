50e0e7ba20e206182d3b7a6b053f9cf43f925c0e
rose-charlotte <charlotte@anros.fr>
2024-09-05 10:52:25 +0200

    reset Home.tsx

--- apps/admin/src/pages/home/Home.tsx
+++ apps/admin/src/pages/home/Home.tsx
@@ -1,91 +1,17 @@
+import { Link } from "react-router-dom";
+import style from "./Home.module.css";
+
 export function Home() {
-    const userId = "toto@toto.com";
-    const userPassword = "toto";
-
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const data = new FormData(e.target as HTMLFormElement);
-        const email = data.get("id");
-        const password = data.get("password");
-
-        if (password === userPassword && email === userId) {
-            console.log("ok");
-        } else {
-            alert("identifiant ou mot de passe invalide");
-        }
-    };
-
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
-                        EZLOC
-                    </h1>
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Connexion
-                    </h2>
-                </div>
-
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" onSubmit={onSubmit} method="POST" className="space-y-6">
-                        <div>
-                            <label htmlFor="id" className="block text-sm font-medium leading-6 text-gray-900">
-                                Identifiant
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="id"
-                                    name="id"
-                                    type="email"
-                                    required
-                                    autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Password
-                                </label>
-                                <div className="text-sm">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign in
-                            </button>
-                        </div>
-                    </form>
-
-                    <p className="mt-10 text-center text-sm text-gray-500">
-                        Pas encore membre?{" "}
-                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                            Creer votre compte
-                        </a>
-                    </p>
-                </div>
-            </div>
+            <h1>Je suis la Home page</h1>
+            <nav className={style.nav}>
+                <Link to="/">Ajouter une location</Link>
+                <Link to="/">Ajouter un locataire</Link>
+                <Link to="/">Liste des locations</Link>
+                <Link to="/">liste des locataires</Link>
+                <Link to="/">utilisateur</Link>
+            </nav>
         </>
     );
 }
