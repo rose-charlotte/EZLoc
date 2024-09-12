71feadd67ba7d3c0e1523cfb37271d781b579c74
rose-charlotte <charlotte@anros.fr>
2024-09-12 12:16:14 +0200

    set up header

---
+++ apps/admin/src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">EZLOC</h1>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Connexion
+                </h2>
+            </div>
+        </div>
+    );
+}
