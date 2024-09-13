02beccff775e724b6563f4c144b105326e649978
rose-charlotte <charlotte@anros.fr>
2024-09-13 09:40:15 +0200

    add uselocation to header

--- apps/admin/src/components/header/Header.tsx
+++ apps/admin/src/components/header/Header.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 export function Header() {
+    const [title, setTitle] = useState("");
+    const location = useLocation();
+    const locatedPage = location.pathname;
+
+    useEffect(() => {
+        switch (locatedPage) {
+            case "/":
+                setTitle("connexion");
+                break;
+            case "/location":
+                setTitle("location");
+                break;
+            case "/locataire":
+                setTitle("locataire");
+                break;
+            default:
+                setTitle("");
+        }
+    }, [locatedPage]);
+
+    console.log(location.pathname);
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">EZLOC</h1>
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Connexion
-                </h2>
+                {locatedPage && (
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        {title}
+                    </h2>
+                )}
             </div>
         </div>
     );
