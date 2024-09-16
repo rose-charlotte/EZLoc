60f789313c554e15690ab2bed2f27ddc72171125
rose-charlotte <charlotte@anros.fr>
2024-09-16 12:46:05 +0200

    modif + css

--- apps/admin/src/pages/signIn/SignInForm.tsx
+++ apps/admin/src/pages/signIn/SignInForm.tsx
@@ -1,8 +1,10 @@
 import { Form } from "../../components/Form/Form";
 import { FormField } from "../../components/Form/FormField";
 import { SignIn } from "models";
+import style from "./SignIn.module.css";
 
 export function SignInForm() {
+    //A EXTERNALISER
     async function postInfo(info: object) {
         const response = await fetch(import.meta.env.VITE_API_ROOT + "user", {
             method: "POST",
@@ -13,25 +15,18 @@ export function SignInForm() {
         console.log("la reponse du back", mss);
     }
     const onSubmit = (signIn: SignIn) => {
-        console.log(`logged with email: ${signIn.email} and password: ${signIn.password}`);
-        console.log(signIn);
-
         postInfo({ email: signIn.email, password: signIn.password });
     };
 
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
-                        EZLOC
-                    </h1>
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Connexion
-                    </h2>
+            <div className={style.container}>
+                <div className={style.titleContainer}>
+                    <h1 className={style.mainTitle}>EZLOC</h1>
+                    <h2 className={style.title}>Connexion</h2>
                 </div>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div className={style.formContainer}>
                     <Form<SignIn> onSubmit={onSubmit} submitLabel="Connexion">
                         <FormField<SignIn> label="Identifiant" name="email" required type="email" />
                         <FormField<SignIn>
@@ -47,7 +42,7 @@ export function SignInForm() {
                         />
                     </Form>
 
-                    <p className="mt-10 text-center text-sm text-gray-500">
+                    <p className={style.p}>
                         Pas encore membre? <a href="#">Créer votre compte</a>
                     </p>
                 </div>
