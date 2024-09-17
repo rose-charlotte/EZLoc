fdda16e9a1e0ab4c08a892f08ef246caa2c190ad
rose-charlotte <charlotte@anros.fr>
2024-09-17 12:42:52 +0200

    setUp selectfield component

---
+++ apps/admin/src/components/Form/SelectField.tsx
@@ -0,0 +1,52 @@
+import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+export function SelectField<T>(props: SelectFieldProps<T>) {
+    return (
+        <Listbox value={props.value} onChange={props.onChange}>
+            <Label className="block text-sm font-medium leading-6 text-gray-900">{props.label}</Label>
+            <div className="relative mt-2">
+                <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                    <span className="flex items-center">
+                        <span className="ml-3 block truncate">{String(props.value)}</span>
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                        <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+                    </span>
+                </ListboxButton>
+
+                <ListboxOptions
+                    transition
+                    className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+                >
+                    {props.options.map(element => (
+                        <ListboxOption
+                            key={String(element)}
+                            value={element}
+                            className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                        >
+                            <div className="flex items-center">
+                                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
+                                    {String(element)}
+                                </span>
+                            </div>
+
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                                <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                            </span>
+                        </ListboxOption>
+                    ))}
+                </ListboxOptions>
+            </div>
+        </Listbox>
+    );
+}
+
+export interface SelectFieldProps<T> {
+    label: string;
+    name: string;
+    options: T[];
+    value?: T;
+    onChange?: (value: T) => void;
+    required: boolean;
+}
