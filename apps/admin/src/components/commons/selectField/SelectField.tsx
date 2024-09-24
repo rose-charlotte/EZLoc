7501d3392a250c51a3cec76ee2b50fe9247bd355
rose-charlotte <charlotte@anros.fr>
2024-09-24 10:35:01 +0200

    add css selectfield enlement

--- apps/admin/src/components/commons/selectField/SelectField.tsx
+++ apps/admin/src/components/commons/selectField/SelectField.tsx
@@ -5,17 +5,19 @@ export function SelectField(props: SelectFieldProps) {
     const selectId = useId();
     return (
         <>
-            <div className={style.labelContainer}>
-                <label htmlFor={selectId} className={style.label}>
-                    {props.label}
-                </label>
-            </div>
-            <div className={style.fieldContainer}>
-                <select id={selectId} name={props.name} className={style.select}>
-                    {props.options.map(option => {
-                        return <option key={option.id}>{option.name}</option>;
-                    })}
-                </select>
+            <div className={style.container}>
+                <div className={style.labelContainer}>
+                    <label htmlFor={selectId} className={style.label}>
+                        {props.label}
+                    </label>
+                </div>
+                <div className={style.fieldContainer}>
+                    <select id={selectId} name={props.name} className={style.select}>
+                        {props.options.map(option => {
+                            return <option key={option.id}>{option.name}</option>;
+                        })}
+                    </select>
+                </div>
             </div>
         </>
     );
