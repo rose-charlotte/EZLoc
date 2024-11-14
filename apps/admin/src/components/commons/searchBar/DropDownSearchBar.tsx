4c9c21e5396545a3bb9ff2ceefb0e606359d8682
rose-charlotte <charlotte@anros.fr>
2024-11-14 14:43:38 +0100

    modification du dropDownSearch

--- apps/admin/src/components/commons/searchBar/DropDownSearchBar.tsx
+++ apps/admin/src/components/commons/searchBar/DropDownSearchBar.tsx
@@ -1,52 +1,30 @@
 import { useId } from "react";
 import style from "./DropDownSearch.module.css";
 
+import { TextField } from "@mui/material";
+
 export function DropDownSearchBar(props: DropDownSearchBarProps) {
     const id = useId();
 
     return (
         <div className={style.container}>
-            <label htmlFor={id} className={style.label}>
-                Search
-            </label>
-            <div className="relative">
-                <div className={style.svg}>
-                    <svg
-                        className="w-4 h-4 text-gray-500 "
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 20"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                        />
-                    </svg>
-                </div>
-                <input
-                    autoComplete="off"
-                    type="search"
-                    id={id}
-                    className={style.input}
-                    placeholder={props.placeholder}
-                    onClick={props.onClick}
-                    onChange={e => props.onChange(e.currentTarget.value)}
-                    required
-                />
-                <button type="submit" className={style.button}>
-                    Search
-                </button>
-            </div>
+            <TextField
+                autoComplete="off"
+                type="search"
+                id={id}
+                label={props.label}
+                className={style.input}
+                placeholder={props.placeholder}
+                onClick={props.onClick}
+                onChange={e => props.onChange(e.currentTarget.value)}
+            />
         </div>
     );
 }
 
 export interface DropDownSearchBarProps {
     placeholder?: string;
+    label: string;
     onClick: () => void;
     onChange: (value: string) => void;
 }
