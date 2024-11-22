a4575d53eb42f3fd207311ffb7eb9fe2cb437151
rose-charlotte <charlotte@anros.fr>
2024-11-22 13:14:16 +0100

    modification 3

--- apps/admin/src/components/commons/tags/TagsList.tsx
+++ apps/admin/src/components/commons/tags/TagsList.tsx
@@ -8,8 +8,8 @@ export function TagList(props: TagListProps) {
                     color="primary"
                     key={index}
                     label={tag}
-                    onClick={props.handleClick}
-                    onDelete={props.handleDelete}
+                    onClick={() => props.onClick(tag)}
+                    onDelete={() => props.onDelete(tag)}
                 />
             ))}
         </Stack>
@@ -18,6 +18,6 @@ export function TagList(props: TagListProps) {
 
 export interface TagListProps {
     tags: string[];
-    handleClick: () => void;
-    handleDelete: () => void;
+    onClick: (tag: string) => void;
+    onDelete: (tag: string) => void;
 }
